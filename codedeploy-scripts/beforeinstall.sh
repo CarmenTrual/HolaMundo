76ead5499cb28f5b25db0a7fcd86258bb5a1a20b
CarmenTrual <carmen.trujillo.alvarez@hotmail.com>
2024-02-11 13:46:57 +0100

    Update

--- codedeploy-scripts/beforeinstall.sh
+++ codedeploy-scripts/beforeinstall.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-chmod +x /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/codeploy-scripts/beforeInstall.sh
-
 chmod +x /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/gradlew
 
 cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive
-./gradlew build
+./gredlew build
 ./gradlew war
+
+
